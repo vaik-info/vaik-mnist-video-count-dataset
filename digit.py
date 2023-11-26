eaa2785f3fbe639b37a4e27a075bc21299e7df8e
kentaro <k.wakisaka@vaikobo.com>
2023-11-26 20:41:45 +0900

    first commit

---
+++ digit.py
@@ -0,0 +1,134 @@
+import random
+from PIL import Image
+import numpy as np
+class Digit:
+    def __init__(self, org_image, canvas_shape, min_speed, max_speed, char_min_size, char_max_size, min_char_size_ratio, max_char_size_ratio, occlusion_ratio, size_change_ratio=0.1, speed_change_ratio=0.1):
+        self.current_color_image, self.current_mask_image = self.__init_org_image(org_image, char_min_size, char_max_size)
+        self.canvas_shape = canvas_shape
+        self.center_x = random.randint(0, canvas_shape[1]-1)
+        self.center_y = random.randint(0, canvas_shape[0]-1)
+        self.min_speed, self.max_speed = min_speed, max_speed
+        self.current_x_speed = random.randint(self.min_speed, self.max_speed)
+        self.current_y_speed = random.randint(self.min_speed, self.max_speed)
+        self.min_char_size_ratio, self.max_char_size_ratio = min_char_size_ratio, max_char_size_ratio
+        self.current_x_size = random.uniform(self.min_char_size_ratio, self.max_char_size_ratio)
+        self.current_y_size = random.uniform(self.min_char_size_ratio, self.max_char_size_ratio)
+        self.occlusion_ratio = occlusion_ratio
+        self.occlusion_flag = False
+        self.size_change_ratio = size_change_ratio
+        self.speed_change_ratio = speed_change_ratio
+
+    def paste(self, canvas):
+        if self.occlusion_flag:
+            self.__next()
+            return canvas
+
+        paste_start_x = self.center_x - self.current_color_image.shape[1]//2
+        if paste_start_x < 0:
+            crop_start_x = -paste_start_x
+            paste_start_x = 0
+        else:
+            crop_start_x = 0
+        paste_start_y = self.center_y - self.current_color_image.shape[0]//2
+        if paste_start_y < 0:
+            crop_start_y = -paste_start_y
+            paste_start_y = 0
+        else:
+            crop_start_y = 0
+        paste_end_x = paste_start_x + self.current_color_image.shape[1] - crop_start_x
+        if paste_end_x > self.canvas_shape[1]:
+            crop_end_x = self.current_color_image.shape[1] - (paste_end_x - self.canvas_shape[1])
+            paste_end_x = self.canvas_shape[1]
+        else:
+            crop_end_x = self.current_color_image.shape[1]
+        paste_end_y = paste_start_y + self.current_color_image.shape[0] - crop_start_y
+        if paste_end_y > self.canvas_shape[0]:
+            crop_end_y = self.current_color_image.shape[0] - (paste_end_y - self.canvas_shape[0])
+            paste_end_y = self.canvas_shape[0]
+        else:
+            crop_end_y = self.current_color_image.shape[0]
+        paste_image = self.current_color_image[crop_start_y:crop_end_y, crop_start_x:crop_end_x, :]
+        paste_mask_image = self.current_mask_image[crop_start_y:crop_end_y, crop_start_x:crop_end_x]
+        canvas[paste_start_y:paste_end_y, paste_start_x:paste_end_x, :][paste_mask_image > 0] = \
+            paste_image[paste_mask_image > 0]
+        self.__next()
+        return canvas
+
+    def __next(self):
+        self.center_x = self.current_x_speed + self.center_x
+        if self.center_x < 0:
+            self.center_x = 0
+            self.current_x_speed = -self.current_x_speed
+        if self.center_x > self.canvas_shape[1]:
+            self.center_x = self.canvas_shape[1] - 1
+            self.current_x_speed = -self.current_x_speed
+        self.center_y = self.current_y_speed + self.center_y
+        if self.center_y < 0:
+            self.center_y = 0
+            self.current_y_speed = -self.current_y_speed
+        if self.center_y > self.canvas_shape[0]:
+            self.center_y = self.canvas_shape[0] - 1
+            self.current_y_speed = -self.current_y_speed
+
+        self.current_color_image = np.array(Image.fromarray(self.current_color_image).resize((
+            max(4, int(self.current_color_image.shape[1]*self.current_x_size)),
+                max(16, int(self.current_color_image.shape[0]*self.current_y_size)))))
+        self.current_mask_image = np.array(Image.fromarray(self.current_mask_image).resize((
+            max(4, int(self.current_mask_image.shape[1]*self.current_x_size)),
+                max(16, int(self.current_mask_image.shape[0]*self.current_y_size)))))
+
+        if random.uniform(0, 1.0) < self.occlusion_ratio:
+            self.occlusion_flag = not self.occlusion_flag
+
+        if random.uniform(0, 1.0) < self.speed_change_ratio:
+            self.current_x_speed = random.randint(self.min_speed, self.max_speed)
+            self.current_y_speed = random.randint(self.min_speed, self.max_speed)
+
+        if random.uniform(0, 1.0) < self.size_change_ratio:
+            self.current_x_size = random.uniform(self.min_char_size_ratio, self.max_char_size_ratio)
+            self.current_y_size = random.uniform(self.min_char_size_ratio, self.max_char_size_ratio)
+
+    def __init_org_image(self, org_image, char_min_size, char_max_size):
+        mnist_char_image = np.array(Image.fromarray(org_image).resize(
+            (random.randint(char_min_size, char_max_size), (random.randint(char_min_size, char_max_size)))))
+        mnist_char_image = np.clip((mnist_char_image > 125) * 255, 0, 255)
+        mnist_char_image = self.__crop_char_image(mnist_char_image).astype(np.uint8)
+        mnist_color_char_image = np.zeros(mnist_char_image.shape + (3,), dtype=np.uint8)
+        mnist_color_char_image[:, :, 0] = np.clip((mnist_char_image * random.uniform(0., 1.)).astype(np.uint8), 0,
+                                                  255)
+        mnist_color_char_image[:, :, 1] = np.clip((mnist_char_image * random.uniform(0., 1.)).astype(np.uint8), 0,
+                                                  255)
+        mnist_color_char_image[:, :, 2] = np.clip((mnist_char_image * random.uniform(0., 1.)).astype(np.uint8), 0,
+                                                  255)
+        return mnist_color_char_image, mnist_char_image
+
+    def __crop_char_image(self, mnist_char_image):
+        x_min, y_min, x_max, y_max = 0, 0, mnist_char_image.shape[1], mnist_char_image.shape[0]
+
+        x_sum_mnist_char_image = np.sum(mnist_char_image, axis=0)
+        y_sum_mnist_char_image = np.sum(mnist_char_image, axis=1)
+
+        for i in range(x_sum_mnist_char_image.shape[0]):
+            if x_sum_mnist_char_image[i] > 0:
+                x_min = i
+                break
+
+        for i in range(y_sum_mnist_char_image.shape[0]):
+            if y_sum_mnist_char_image[i] > 0:
+                y_min = i
+                break
+
+        x_revert_sum_diff_bool_image = x_sum_mnist_char_image[::-1]
+        y_revert_sum_diff_bool_image = y_sum_mnist_char_image[::-1]
+
+        for i in range(x_revert_sum_diff_bool_image.shape[0]):
+            if x_revert_sum_diff_bool_image[i] > 0:
+                x_max = x_revert_sum_diff_bool_image.shape[0] - i
+                break
+
+        for i in range(y_revert_sum_diff_bool_image.shape[0]):
+            if y_revert_sum_diff_bool_image[i] > 0:
+                y_max = y_revert_sum_diff_bool_image.shape[0] - i
+                break
+
+        return mnist_char_image[y_min:y_max, x_min:x_max]
---
+++ main.py
@@ -0,0 +1,114 @@
+import argparse
+import os
+import random
+import shutil
+import json
+from PIL import Image
+import imageio
+import numpy as np
+import tensorflow as tf
+from tqdm import tqdm
+
+from digit import Digit
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+
+def write(output_sub_dir_path, sample_num, image_max_size, image_min_size, char_max_size, char_min_size, char_max_num,
+          char_min_num, x, y, classes, min_frame_num, max_frame_num, min_char_speed, max_char_speed,
+          min_char_size_ratio, max_char_size_ratio, occlusion_ratio, fps=10):
+    os.makedirs(output_sub_dir_path, exist_ok=True)
+
+    for file_index in tqdm(range(sample_num), desc=f'write at {output_sub_dir_path}'):
+        default_canvas = np.zeros(
+            (random.randint(image_min_size, image_max_size), random.randint(image_min_size, image_max_size), 3),
+            dtype=np.uint8)
+        frame_num = random.randint(min_frame_num, max_frame_num)
+        count_dict = {}
+        # init Digit List
+        digit_list = []
+        canvas = np.zeros(default_canvas.shape, dtype=default_canvas.dtype)
+        for char_index in range(random.randint(char_min_num, char_max_num)):
+            while True:
+                mnist_index = random.randint(0, y.shape[0] - 1)
+                if len(classes) > y[mnist_index]:
+                    break
+            if classes[y[mnist_index]] not in count_dict.keys():
+                count_dict[classes[y[mnist_index]]] = 0
+            count_dict[classes[y[mnist_index]]] += 1
+            digit_list.append(
+                Digit(x[mnist_index], default_canvas.shape, min_char_speed, max_char_speed, char_min_size, char_max_size,
+                      min_char_size_ratio, max_char_size_ratio, occlusion_ratio))
+            canvas = digit_list[-1].paste(canvas)
+
+        # create frames
+        image_array_list = [canvas]
+        for frame_index in range(frame_num):
+            canvas = np.zeros(default_canvas.shape, dtype=default_canvas.dtype)
+            for a_digit in digit_list:
+                canvas = a_digit.paste(canvas)
+            image_array_list.append(canvas)
+
+        file_name = f'{os.path.basename(output_sub_dir_path)}_{file_index:09d}'
+        imageio.mimwrite(os.path.join(output_sub_dir_path, f'{file_name}.avi'), image_array_list, fps=fps, codec='rawvideo')
+        output_json_path = os.path.join(output_sub_dir_path, f'{file_name}.json')
+        with open(output_json_path, 'w') as f:
+            json.dump(count_dict, f)
+
+
+def main(output_dir_path, classes_txt_path, train_sample_num, valid_sample_num, image_max_size, image_min_size,
+         char_max_size,
+         char_min_size, char_max_num, char_min_num, min_frame_num, max_frame_num, min_char_speed, max_char_speed,
+         min_char_size_ratio, max_char_size_ratio, occlusion_ratio):
+    os.makedirs(output_dir_path, exist_ok=True)
+
+    mnist = tf.keras.datasets.mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+    classes = []
+    with open(classes_txt_path) as f:
+        for line in f:
+            classes.append(line.strip())
+
+    output_train_dir_path = os.path.join(output_dir_path, 'train')
+    write(output_train_dir_path, train_sample_num, image_max_size, image_min_size, char_max_size, char_min_size,
+          char_max_num, char_min_num, x_train, y_train, classes, min_frame_num, max_frame_num, min_char_speed,
+          max_char_speed, min_char_size_ratio, max_char_size_ratio, occlusion_ratio)
+
+    output_valid_dir_path = os.path.join(output_dir_path, 'valid')
+    write(output_valid_dir_path, valid_sample_num, image_max_size, image_min_size, char_max_size, char_min_size,
+          char_max_num, char_min_num, x_test, y_test, classes, min_frame_num, max_frame_num, min_char_speed,
+          max_char_speed, min_char_size_ratio, max_char_size_ratio, occlusion_ratio)
+
+    shutil.copy(classes_txt_path, os.path.join(output_dir_path, os.path.basename(classes_txt_path)))
+
+    json_dict = {'classes': classes}
+    with open(os.path.join(output_dir_path, 'classes.json'), 'w') as f:
+        json.dump(json_dict, f, ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': '))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='main')
+    parser.add_argument('--output_dir_path', type=str, default='~/.vaik-mnist-video-count-dataset')
+    parser.add_argument('--classes_txt_path', type=str, default=os.path.join(os.path.dirname(__file__), 'classes.txt'))
+    parser.add_argument('--train_sample_num', type=int, default=10)
+    parser.add_argument('--valid_sample_num', type=int, default=5)
+    parser.add_argument('--image_max_size', type=int, default=320)
+    parser.add_argument('--image_min_size', type=int, default=196)
+    parser.add_argument('--char_max_size', type=int, default=64)
+    parser.add_argument('--char_min_size', type=int, default=32)
+    parser.add_argument('--char_max_num', type=int, default=10)
+    parser.add_argument('--char_min_num', type=int, default=1)
+    parser.add_argument('--min_frame_num', type=int, default=16)
+    parser.add_argument('--max_frame_num', type=int, default=32)
+    parser.add_argument('--min_char_speed', type=int, default=-10)
+    parser.add_argument('--max_char_speed', type=int, default=10)
+    parser.add_argument('--min_char_size_ratio', type=int, default=0.98)
+    parser.add_argument('--max_char_size_ratio', type=int, default=1.02)
+    parser.add_argument('--occlusion_ratio', type=int, default=0.05)
+    args = parser.parse_args()
+
+    args.classes_txt_path = os.path.expanduser(args.classes_txt_path)
+    args.output_dir_path = os.path.expanduser(args.output_dir_path)
+
+    main(**args.__dict__)
